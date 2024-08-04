485590c9bdba710bc1bac9fb8bfe989d11cfbb08
Mohd Zaid <mdzaid0205@gmail.com>
2024-08-04 19:20:40 +0530

    added date plugin and fixed margins

--- Console/Plugins.py
+++ Console/Plugins.py
@@ -4,3 +4,5 @@ from datetime import datetime
 def getCurrentTime() -> str:
     return f"{datetime.now().strftime("%H:%M:%S")}".center(12)
 
+def getCurrentDate() -> str:
+    return f"{datetime.now().strftime("%d:%b:%y")}".center(13)
