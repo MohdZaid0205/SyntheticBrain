7cc755180f5edb4fddfe7176e4a68552505cadb1
Mohd Zaid <mdzaid0205@gmail.com>
2024-08-04 17:03:25 +0530

    Add Alias to put Badge in front of Console outputs

---
+++ Console/Alias.py
@@ -0,0 +1,21 @@
+
+from .Colors import Color, Colored
+from dataclasses import dataclass
+from typing import Optional
+
+
+class Alias:
+
+    def __init__( 
+            self, name:str, width:int,
+            fore:Optional[Color] = None,
+            back:Optional[Color] = None,
+            active:Optional[bool] = True 
+        ) -> None:
+        self.__is_color_active = active
+        self.__uncolored_alias = f"[{ name.title().center( width - 2 ) }]"
+        self.__colored_alias   = Colored.coloredText(self.__uncolored_alias, fore, back)
+
+    def __str__[ ColoredAlias ]( self ) -> ColoredAlias:
+        return self.__colored_alias if self.__is_color_active else self.__uncolored_alias
+    
