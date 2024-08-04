f8af7b8d26da8d674725bf84906e5804a5f017ad
Mohd Zaid <mdzaid0205@gmail.com>
2024-08-04 16:39:16 +0530

    Add Colored for handeling ANSI and coloring texts

--- Console/Colors.py
+++ Console/Colors.py
@@ -31,3 +31,26 @@ class Color:
             a:float = int(hex[6:8], 16) / 255.0
         
         return cls( r, g, b, a )
+
+
+class Colored:
+
+    FOREGROUND  = "\033[38;2;{r};{g};{b}m"
+    BACKGROUND  = "\033[48;2;{r};{g};{b}m"
+    END_COLORS  = "\033[0m"
+
+    @staticmethod
+    def coloredForeground[ ColoredString ]( text:str, foreColor:Color ) -> ColoredString:
+        color_sequence:str = Colored.FOREGROUND.format( r=foreColor.r, g=foreColor.g, b=foreColor.b )
+        return f"{color_sequence}{text}{Colored.END_COLORS}"
+
+    @staticmethod
+    def coloredBackground[ ColoredString ]( text:str, backColor:Color ) -> ColoredString:
+        color_sequence:str = Colored.BACKGROUND.format( r=backColor.r, g=backColor.g, b=backColor.b )
+        return f"{color_sequence}{text}{Colored.END_COLORS}"
+    
+    @staticmethod
+    def coloredText[ ColoredString ]( text:str, foreColor:Color, backColor:Color ) -> ColoredString:
+        f_color_sequence:str = Colored.FOREGROUND.format( r=foreColor.r, g=foreColor.g, b=foreColor.b )
+        b_color_sequence:str = Colored.BACKGROUND.format( r=backColor.r, g=backColor.g, b=backColor.b )
+        return f"{f_color_sequence}{b_color_sequence}{text}{Colored.END_COLORS}"
